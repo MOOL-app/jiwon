ff5446882767802f6567a34668a5ccbf7e9ba1cf
jiwonLee42 <ljw56b@gmail.com>
2023-12-10 22:47:21 +0900

    edit HelloController

--- src/main/java/mool/moolapp/MoolApplication.java
+++ src/main/java/mool/moolapp/MoolApplication.java
@@ -1,7 +1,7 @@
 package mool.moolapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class MoolApplication {
 
-	@GetMapping("/helloo")
-	public String hello() {
+	@RequestMapping("/helloo")
+	String hello() {
 		return "hello";
 	}
 	public static void main(String[] args) {
