75318d8e0add03177acafbd34e61da797a1a81c1
jiwonLee42 <ljw56b@gmail.com>
2023-12-13 14:04:56 +0900

    edit Hellocontroller

--- src/main/java/mool/moolapp/MoolApplication.java
+++ src/main/java/mool/moolapp/MoolApplication.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class MoolApplication {
 
-	@RequestMapping("/helloo")
+	@RequestMapping("/hello")
 	String hello() {
 		return "hello";
 	}
